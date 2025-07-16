4003f5fd2e6a20602ce99739b45757f46d5122b8
CCRICE <2371092140@qq.com>
2025-07-16 21:53:26 +0800

    添加交流群

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,12 @@ export default defineConfig({
 	icon(),
 		starlight({
 			title: '超级工大',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/cc2562/superhut' }],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/cc2562/superhut' },
+			{ icon: 'github', label: 'GitHub', href: 'https://github.com/cc2562/superhut' },
+			],
 			sidebar: [
 				{
 					label: '文档',
-					
 					autogenerate: { directory: 'upgrade' },
 				},
 				{
@@ -21,6 +22,7 @@ export default defineConfig({
 						// Each item here is one entry in the navigation menu.
 						{ label: 'GitHub仓库', link: 'https://github.com/cc2562/superhut' },
 						{ label: '软件下载', link: 'https://down.ccrice.com/#s/superhut/' },
+						{ label: 'QQ交流群', link: 'https://qm.qq.com/q/stCS8sY3jW' },
 					],
 				},
 				
